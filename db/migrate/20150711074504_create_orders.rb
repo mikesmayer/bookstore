4497948ecfbca34ccdeb612e6912222e3fe82390
Artem Zaslavskiy <artem.zaslavskiy@gmail.com>
2015-07-27 22:24:45 +0300

    add references

--- db/migrate/20150711074504_create_orders.rb
+++ db/migrate/20150711074504_create_orders.rb
@@ -3,8 +3,8 @@ class CreateOrders < ActiveRecord::Migration
     create_table :orders do |t|
       t.decimal :total_price
       t.datetime :completed_date
-      t.references :billing_address, index: true, foreign_key: true
-      t.references :shipping_address, index: true, foreign_key: true
+      t.references :billing_address, references: :addresses, index: true, foreign_key: true
+      t.references :shipping_address, references: :addresses, index: true, foreign_key: true
       t.string :status
       t.references :customer, index: true, foreign_key: true
       t.references :credit_card, index: true, foreign_key: true
